00c09bb6b1db81b80b1e218f9e965c84cf0df245
scarygami@gmail.com <>
2013-06-10 13:28:45 +0000

    Finalize background replacement

--- apps/background/background.js
+++ apps/background/background.js
@@ -11,7 +11,15 @@
       scene,
       camera,
       last_tick = new Date().getTime(),
-      update_interval = 100;
+      camera_tick = last_tick,
+      update_tick = last_tick,
+      update_interval = 100,
+      camera_interval = 2000,
+      ROT_90 = Math.PI / 2,
+      speed = 1 / 2000,
+      dif_x = speed, dif_y = speed, dif_z = speed,
+      backgroundReplacement,
+      backgroundResource;
 
     if (global.gapi && global.gapi.hangout) {
       hapi = global.gapi.hangout;
@@ -26,9 +34,34 @@
     function tick() {
       var
         this_tick = new Date().getTime(),
-        elapsed = this_tick - last_tick;
+        elapsed = this_tick - last_tick,
+        oldResource;
 
       // TODO: animate camera
+      camera.rotation.x += elapsed * dif_x;
+      camera.rotation.y += elapsed * dif_y;
+      camera.rotation.z += elapsed * dif_z;
+
+      if (this_tick - update_tick > update_interval) {
+        // Update background resource
+        oldResource = backgroundResource;
+        backgroundResource = hapi.av.effects.createImageResource(renderer.domElement.toDataURL("image/png"));
+        backgroundReplacement.setImageResource(backgroundResource);
+        if (!backgroundReplacement.isVisible()) {
+          backgroundReplacement.setVisible(true);
+        }
+        if (!!oldResource) {
+          oldResource.dispose();
+        }
+        update_tick = this_tick;
+      }
+      
+      if (this_tick - camera_tick > camera_interval) {
+        dif_x = Math.random() * speed;
+        dif_y = Math.random() * speed;
+        dif_z = Math.random() * speed;
+        camera_tick = this_tick;
+      }
 
       renderer.render(scene, camera);
       last_tick = this_tick; 
@@ -36,7 +69,7 @@
     }
 
     function initialize() {
-      var room, tmp, i, colors = ["yellow", "red", "blue"], canvas, ctx, y, step, texture, material, mesh;
+      var room, tmp, i, colors = ["yellow", "red", "blue"], canvas, ctx, y, step, texture, material, mesh, size, half;
 
       room = document.getElementById("room")
       
@@ -51,26 +84,33 @@
       renderer.setClearColor(new THREE.Color(0xCCCCCC));
       
       scene = new THREE.Scene();
-      camera = new THREE.PerspectiveCamera(45, 3 / 2, 0.1, 10000);
-      camera.position.z = 300;
+      camera = new THREE.PerspectiveCamera(50, 3 / 2, 0.1, 10000);
+      camera.position.x = 0;
+      camera.position.y = 0;
+      camera.position.z = 0;
       scene.add(camera);
       
       tmp = new THREE.AmbientLight(0x333333);
       scene.add(tmp);
       
+      
+      size = 256;
+      half = size / 2
       // create wall textures
       for (i = 0; i < 3; i++) {
         canvas = document.getElementById("texture" + (i + 1));
+        canvas.width = size;
+        canvas.height = size;
         ctx = canvas.getContext("2d");
         ctx.fillStyle = "black";
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        step = Math.floor(canvas.height / 50);
+        ctx.fillRect(0, 0, size, size);
+        step = Math.floor(size / 50);
         ctx.strokeStyle = colors[i];
         ctx.lineWidth = 2;
-        for (y = Math.floor(step / 2); y < canvas.height; y+= step) {
+        for (y = Math.floor(step / 2); y < size; y+= step) {
           ctx.beginPath();
           ctx.moveTo(0, y);
-          ctx.lineTo(canvas.width, y);
+          ctx.lineTo(size, y);
           ctx.stroke();
           ctx.closePath();
         }
@@ -78,24 +118,55 @@
         texture.needsUpdate = true;
         material = new THREE.MeshBasicMaterial({map: texture});
 
-        mesh = new THREE.Mesh(new THREE.PlaneGeometry(canvas.width, canvas.height), material);				
+        mesh = new THREE.Mesh(new THREE.PlaneGeometry(size, size), material);				
         mesh.doubleSided = true;
-        mesh.position.x = -canvas.width / 2;
-        mesh.position.y = -canvas.height / 2;
-        mesh.position.z = canvas.width / 2;
+        switch (i) {
+          case 0:
+            mesh.rotation.x = -ROT_90;
+            mesh.position.y = -half;
+            break;
+          case 1:
+            mesh.rotation.y = ROT_90;
+            mesh.position.x = -half;
+            break;
+          case 2:
+            mesh.rotation.y = 0;
+            mesh.position.z = -half;
+            break;
+        }
         scene.add(mesh);
         
-        mesh = new THREE.Mesh(new THREE.PlaneGeometry(canvas.width, canvas.height), material);  			
+        mesh = new THREE.Mesh(new THREE.PlaneGeometry(size, size), material);  			
         mesh.doubleSided = true;
-        mesh.position.x = -canvas.width / 2;
-        mesh.position.y = -canvas.height / 2;
-        mesh.position.z = -canvas.width / 2;
+        switch (i) {
+          case 0:
+            mesh.rotation.x = ROT_90;
+            mesh.position.y = half;
+            break;
+          case 1:
+            mesh.rotation.y = -ROT_90;
+            mesh.position.x = half;
+            break;
+          case 2:
+            mesh.rotation.y = 2 * ROT_90;
+            mesh.position.z = half;
+            break;
+        }
         scene.add(mesh);
       }
       
       room.appendChild(renderer.domElement);
       
-      tick();
+      hapi.av.effects.requestBackgroundReplacementLock(function (success) {
+        if (success) {
+          console.log("Got BackgroundReplacementLock.");
+          backgroundReplacement = hapi.av.effects.getBackgroundReplacement();
+          backgroundReplacement.setAlphaThreshold(150);
+          tick();
+        } else {
+          console.log("Requesting BackgroundReplacementLock failed.");
+        }
+      });
     }
 
     hapi.onApiReady.add(function (e) {
