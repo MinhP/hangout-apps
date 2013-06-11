2808db57eaddbd45f2341771826b1935bbb35cec
scarygami@gmail.com <>
2013-06-11 22:34:41 +0000

    Implement very simple handtracking example

--- apps/handtracking/handtracking.js
+++ apps/handtracking/handtracking.js
@@ -1,20 +1,44 @@
 (function (global) {
   "use strict";
-  var hapi, window = global.window, document = global.document, console = global.console;
-  if (global.gapi && global.gapi.hangout) {
-    hapi = global.gapi.hangout;
-  }
 
   function App() {
+    var
+      hapi,
+      document = global.document,
+      console = global.console,
+      debug = document.getElementById("debug"),
+      overlay;
+    
+    if (global.gapi && global.gapi.hangout) {
+      hapi = global.gapi.hangout;
+    } else {
+      console.log("Hangout API not found...");
+      return;
+    }
+
+    function handtracking(data) {
+      if (data.hasHand) {
+        debug.innerHTML = JSON.stringify(data.hands, undefined, "  ");
+        overlay.setVisible(true);
+      } else {
+        overlay.setVisible(false);
+      }
+    }
 
     function initialize() {
-
+      var resource = hapi.av.effects.createImageResource("https://hangout-apps.appspot.com/handtracking/coin.png");
+      overlay = resource.createHandTrackingOverlay({
+        trackingFeature: hapi.av.effects.HandTrackingFeature.HAND_CENTER,
+        offset: {x: 0, y: 0.1},
+        scale: 0.5,
+        scaleWithHand: false});
+      hapi.av.effects.onHandTrackingDataChanged.add(handtracking);
     }
 
     hapi.onApiReady.add(function (e) {
       if (e.isApiReady) {
         console.log("Hangout API ready!");
-        window.setTimeout(initialize, 1);
+        global.setTimeout(initialize, 1);
       }
     });
   }
