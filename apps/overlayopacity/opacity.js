d648b2bb460d6a3df30eb73a5dfb0ab28f1c526d
Scarygami <scarygami@gmail.com>
2013-07-30 18:38:59 +0200

    Prepare overlay opacity demo

---
+++ apps/overlayopacity/opacity.js
@@ -0,0 +1,57 @@
+(function (global) {
+  "use strict";
+
+  function App() {
+    var
+      hapi,
+      doc = global.document,
+      console = global.console,
+      debug = doc.getElementById("debug"),
+      main = doc.getElementById("opacity"),
+      effects;
+
+    if (global.gapi && global.gapi.hangout) {
+      hapi = global.gapi.hangout;
+    } else {
+      console.log("Hangout API not found...");
+      return;
+    }
+
+    function startEffect(e) {
+      console.log(e);
+    }
+
+    function initialize() {
+      var i, button;
+
+      effects = [
+        {
+          "name": "lightning",
+          "label": "Lightning"
+        },
+        {
+          "name": "eyes",
+          "label": "Flashing Eyes"
+        }
+      ];
+
+      for (i = 0; i < effects.length; i++) {
+        button = doc.createElement(button);
+        button.innerHTML = effects[i].label;
+        button.setAttribute("data-index", i);
+        main.appendChild(button);
+        button.onclick = startEffect;
+      }
+    }
+
+    hapi.onApiReady.add(function (e) {
+      if (e.isApiReady) {
+        console.log("Hangout API ready!");
+        global.setTimeout(initialize, 1);
+      }
+    });
+  }
+
+  global.hangoutapp = new App();
+
+}(this));
